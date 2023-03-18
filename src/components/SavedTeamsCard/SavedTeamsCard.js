438e39d9f5051d13266b0b06356d424e9521c636
ionb23 <ion.borziac@gmail.com>
2023-03-18 19:03:09 +0000

    adding react-search-autocomplete npm package and new autocomplete component

--- src/components/SavedTeamsCard/SavedTeamsCard.js
+++ src/components/SavedTeamsCard/SavedTeamsCard.js
@@ -1,27 +1,62 @@
+import AutoComplete from "../AutoComplete/AutoComplete.js";
+
+
 function SavedTeamsCard(props) {
     const { name, league, leaguePosition, currentPoints, goalsFor, goalsAgainst, goalDifference, newsArticle1, newsArticle2, newsArticle3, iconSrc } = props
 
     return (
         <>
-            <div class="card">
-                <h4>{name}</h4>
-                <img src= {iconSrc} class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-title">Statistics</h5>
-                        <p>League: {league}</p>
-                        <p class="card-text">League Position: {leaguePosition}</p>
-                        <p class="card-text">Current Points: {currentPoints}</p>
-                        <p class="card-text">Goals For: {goalsFor}</p>
-                        <p class="card-text">Goals Against: {goalsAgainst}</p>
-                        <p class="card-text">Goals Difference: {goalDifference}</p>
-                        <hr/>
-                        <h5>News</h5>
-                        <p class="card-text">{newsArticle1}</p>
-                        <p class="card-text">{newsArticle2}</p>
-                        <p class="card-text">{newsArticle3}</p>
-                       
+            <div className="card-group me-5 ms-5">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Arsenal</h5>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
+                </div>
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Records</h5>
+                        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Top Goalscorer</h5>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                </div>
             </div>
+            {/* remove bottom border rounding from team card and remove top border rounding from the comparison div below*/}
+            <div className="card card-footer me-5 ms-5">
+                <small className="text-muted">Choose a team to compare your favourite team against:</small>
+                <AutoComplete />
+            </div>
+
+
+            <div className="card">
+                <h4>{name}</h4>
+                <img src={iconSrc} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Statistics</h5>
+                    <p>League: {league}</p>
+                    <p className="card-text">League Position: {leaguePosition}</p>
+                    <p className="card-text">Current Points: {currentPoints}</p>
+                    <p className="card-text">Goals For: {goalsFor}</p>
+                    <p className="card-text">Goals Against: {goalsAgainst}</p>
+                    <p className="card-text">Goals Difference: {goalDifference}</p>
+                    <hr />
+                    <h5>News</h5>
+                    <p className="card-text">{newsArticle1}</p>
+                    <p className="card-text">{newsArticle2}</p>
+                    <p className="card-text">{newsArticle3}</p>
+                </div>
+            </div>
+
+
+
         </>
     )
 }
--- src/components/SavedTeamsPage/SavedTeamsPage.js
+++ src/components/SavedTeamsPage/SavedTeamsPage.js
@@ -1,33 +1,152 @@
 import SavedTeamsCard from "../SavedTeamsCard/SavedTeamsCard";
+const teamsJSON = require('../../assets/teams-list.json');
 
-function SavedTeamPage() {
-   
-    return (
 
-        <Container id="projects-section">
-          <h1 id="projects-title">Projects</h1>
-    
-          {projects.map((project, index) => {
-            // const image = require(screenshotLink)
-    
-            return (
-    
-              <SavedTeamsCard
-                key={index}
-                name={project.name}
-                description={project.description}
-                deployedLink={project.deployedApp}
-                repoLink={project.githubRepo}
-                screenshot={project.screenshot} />
-            );
-            
-          })}
-    
-        </Container>
-    
-      )
+// import axios from "axios";
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://football-web-pages1.p.rapidapi.com/match.json',
+//   params: {match: '407893'},
+//   headers: {
+//     'X-RapidAPI-Key': '57819f5f43mshbc3a6f975f0fbeep1fa157jsn0811a1b4ba57',
+//     'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+// works but we need the array to contain objects of team+id
+// const teamArray = [];
+
+// for (let i = 0; i < teamsJSON.competitions.length; i++) {
+//     for (let j = 0; j < teamsJSON.competitions[i].teams.length; j++) {
+//         teamArray.push(teamsJSON.competitions[i].teams[j].name)
+//     }
+// }
+
+// const teamArray = [];
+
+// for (let i = 0; i < teamsJSON.competitions.length; i++) {
+//     for (let j = 0; j < teamsJSON.competitions[i].teams.length; j++) {
+//       let teamObj = {
+//         name: teamsJSON.competitions[i].teams[j].name,
+//         id: teamsJSON.competitions[i].teams[j].id
+//       }
+//         teamArray.push(teamObj);
+//     }
+// }
+
+// console.log(teamArray);
+
+
+let inputTeamID;
+let inputTeamComp;
+
+// loops through JSON file to find the team id based on team input name
+function findTeamIDByName(inputTeamName) {
+    for (let i = 0; i < teamsJSON.competitions.length; i++) {
+        for (let j = 0; j < teamsJSON.competitions[i].teams.length; j++) {
+            if (teamsJSON.competitions[i].teams[j].name === inputTeamName) {
+                inputTeamID = teamsJSON.competitions[i].teams[j].id;
+                console.log(inputTeamID);
+            }
+        }
+    }
+}
+
+// loops through JSON file to find the team competition based on team input name
+function findTeamCompByName(inputTeamName) {
+    for (let i = 0; i < teamsJSON.competitions.length; i++) {
+        for (let j = 0; j < teamsJSON.competitions[i].teams.length; j++) {
+            if (teamsJSON.competitions[i].teams[j].name === inputTeamName) {
+                inputTeamComp = teamsJSON.competitions[i].fullName;
+                console.log(inputTeamComp);
+            }
+        }
+    }
+}
+
+findTeamIDByName('Manchester United');
+findTeamCompByName('Manchester United');
+
+
+
+function SavedTeamsPage() {
+  return (
+  <div>
+
+{/* navbar */}
+<nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="https://github.com">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="https://github.com">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="https://github.com">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="https://github.com">Pricing</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+{/* saved teams buttons */}
+<nav className="navbar bg-light me-5 ms-5">
+  <form className="container-fluid justify-content-start">
+    {/* change to loop through local storage saved teams array to create a button for each team */}
+    <button className="btn btn-outline-success me-2" type="button">Team 1</button>
+    <button className="btn btn-outline-success me-2" type="button">Team 2</button>
+    <button className="btn btn-outline-success me-2" type="button">Team 3</button>
+    <button className="btn btn-outline-success me-2" type="button">Team 4</button>
+    <button className="btn btn-outline-success me-2" type="button">Team 5</button>
+  </form>
+</nav>
+
+{/* change to loop through local storage saved teams array to create a card for each team */}
+<SavedTeamsCard />
+
+
+</div>
+)
 
 
 
 }
-export default SavedTeamPage
+export default SavedTeamsPage;
+
+
+
+  // <Container id="projects-section">
+        //   <h1 id="projects-title">Projects</h1>
+    
+        //   {projects.map((project, index) => {
+        //     // const image = require(screenshotLink)
+    
+        //     return (
+    
+        //       <SavedTeamsCard
+        //         key={index}
+        //         name={project.name}
+        //         description={project.description}
+        //         deployedLink={project.deployedApp}
+        //         repoLink={project.githubRepo}
+        //         screenshot={project.screenshot} />
+        //     );
+            
+        //   })}
+    
+        // </Container>
