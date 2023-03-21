b401a7805c6550a5d518856a44d0640ec694f818
ionb23 <ion.borziac@gmail.com>
2023-03-21 17:56:18 +0000

    removing unneeded API call from App.js file

--- src/App.js
+++ src/App.js
@@ -4,25 +4,6 @@ import {HashRouter as Router ,Route, Routes, Navigate} from 'react-router-dom'
 import SavedTeamPage from './components/SavedTeamsPage/SavedTeamsPage';
 import SummaryPage from './components/SummaryPage/SummaryPage';
 
-import axios from "axios";
-
-const options = {
-  method: 'GET',
-  url: 'https://football-web-pages1.p.rapidapi.com/match.json',
-  params: {match: '407893'},
-  headers: {
-    'X-RapidAPI-Key': '57819f5f43mshbc3a6f975f0fbeep1fa157jsn0811a1b4ba57',
-    'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
-
 function App() {
 const pickedTeam = localStorage.getItem('Fav team')
 
